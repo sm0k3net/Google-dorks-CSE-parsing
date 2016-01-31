82e5873b9b400b460ee530120b393f3d30de17f9
sm0k3 <sm0k3net@users.noreply.github.com>
2016-01-31 12:47:05 +0300

    Create gSpider_list.php

---
+++ gSpider_list.php
@@ -0,0 +1,23 @@
+<?php
+//gSpider google 'inurl' dorks list
+$gSpider_list = array(
+'php?id=',
+'.php?id=',
+'?id=',
+'id=',
+'php?show=',
+'archive.php?id=',
+'article.php?id=',
+'browse.php?catid=',
+'catalog.php',
+'category.php',
+'events.php?ID=',
+'index.php?cart=',
+'price.php',
+'?category=',
+'gallery.php?id=',
+'category.php?id=',
+'news.php?id=',
+'order.php?id='
+);
+?>
